bb0228852c8b4d3f719bbeb6a1ee51e83d60b5d6
Kamil Kruk <5230@htl.rennweg.at>
2020-03-03 17:21:38 +0100

    initial setup

---
+++ src/Engine/Engine.java
@@ -0,0 +1,7 @@
+package Engine;
+
+public class Engine {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Simulation_Environment/Simulation_Environment.java
@@ -0,0 +1,7 @@
+package Simulation_Environment;
+
+public class Simulation_Environment {
+    public static void main(String[] args) {
+
+    }
+}
