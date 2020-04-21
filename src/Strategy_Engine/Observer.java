447533a3a108cd9a420b045df1493b9c41c6c402
Kamil Kruk <5230@htl.rennweg.at>
2020-04-21 15:28:58 +0200

    initialize observer class

--- src/Strategy_Engine/Observer.java
+++ src/Strategy_Engine/Observer.java
@@ -1,4 +1,29 @@
 package Strategy_Engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Observer {
+    private int decksInShoe;
+
+    private List<Integer> deck = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
+    private ArrayList<Integer> shoe = new ArrayList<>();
+    private ArrayList<Integer> myHand = new ArrayList<>();
+    private int dealer;
+
+
+    public Observer(int decksInShoe){
+        this.decksInShoe = decksInShoe;
+
+        for (int i = 0; i < decksInShoe; i++) {
+            shoe.addAll(deck);
+        }
+
+        System.out.println(shoe);
+    }
+
+
+    public ArrayList<Integer> getShoe(){
+        return shoe;
+    }
 }
---
+++ src/Strategy_Engine/Strategy_Engine.java
@@ -0,0 +1,8 @@
+package Strategy_Engine;
+
+public class Strategy_Engine {
+    public static void main(String[] args) {
+        Observer o = new Observer(3);
+
+    }
+}
