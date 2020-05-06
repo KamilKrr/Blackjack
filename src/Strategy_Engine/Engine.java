db2aebe10564ca29b4e301d9dc6771949ba7a063
Felix5234 <5234@htl.rennweg.at>
2020-05-06 20:01:19 +0200

    player probabilities

--- src/Strategy_Engine/Engine.java
+++ src/Strategy_Engine/Engine.java
@@ -1,63 +1,71 @@
 package Strategy_Engine;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Engine {
     public static HashMap<Integer, Double> dealer = new HashMap<>();
 
-    public static String bestMove(Shoe shoe, int dealerCard, int playerSum, boolean isPlayerSoftHand) {
+    public static String bestMove(Shoe shoe, int dealerCard, int playerSum, boolean isPlayerSoftHand, List<String> moves) {
         for (int i = 17; i <= 22; i++) {
             dealer.put(i, 0.0);
         }
+        HashMap<Integer, Double> probabilities = dealerProbabilities(shoe, dealerCard, 1.0, dealer, isPlayerSoftHand);
+        System.out.println(probabilities);
 
-        HashMap<Integer, Double> probs = dealerProbabilities(shoe, dealerCard, 1.0, dealer, isPlayerSoftHand);
-
-        System.out.println(probs);
-        double p = 0;
-        for (Double value: probs.values()){
-            p+= value;
-
+        if (moves.contains("insurance")) {
+            if(insuranceEV(shoe) > 0) { return "insurance"; }
+            return "no insurance";
         }
 
-        System.out.println(p);
+        double h = playerHitEV(shoe, probabilities, playerSum, isPlayerSoftHand);
+        double s = playerStandEV(probabilities, playerSum);
+        int count = 2;
+        double d = 0;
+        double p = 0;
+        if (moves.contains("double")) { d = playerDoubleEV(shoe, probabilities, playerSum, isPlayerSoftHand); count++; }
+        if (moves.contains("split")) { p = playerSplitEV(shoe, probabilities, playerSum, isPlayerSoftHand); count++; }
 
-        return "hit";
+        if ((count == 2 && h > s) || (count == 3 && h > s && h > d) || (count == 4 && h > s && h > d && h > p )) { return "hit"; }
+        if ((count == 2 && s > h) || (count == 3 && s > h && s > d) || (count == 4 && s > h && s > d && s > p )) { return "split"; }
+        if ((count == 3 && d > h && d > s) || (count == 4 && d > h && d > s && d > p )) { return "double"; }
+        if (count == 4 && p > h && p > s && p > d) { return "split"; }
+
+        return "error";
     }
 
     public static HashMap<Integer, Double> dealerProbabilities(Shoe shoe, int dealerCard, double probability, HashMap<Integer, Double> probabilities, boolean softHand) {
         //exit condition
-        if(probability == 0) return probabilities;
+        if (probability == 0) return probabilities;
 
-        if(!softHand) {
-            if(dealerCard > 10) {
+        if (!softHand) {
+            if (dealerCard > 10) {
                 for (int i = dealerCard + 1; i <= 16; i++) {
                     Shoe s = new Shoe(shoe);
                     s.remove(i - dealerCard);
-                    dealerProbabilities(s, i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability, probabilities, false);
+                    dealerProbabilities(s, i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability, probabilities, false);
                 }
                 for (int i = 17; i <= 21; i++) {
-                    probabilities.put(i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability + probabilities.get(i));
+                    probabilities.put(i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability + probabilities.get(i));
                 }
-                probabilities.put(22, shoe.countEqualsOrHigher(22 - dealerCard)*1.0 / shoe.getCards().size() * probability + probabilities.get(22));
+                probabilities.put(22, shoe.countEqualsOrHigher(22 - dealerCard) * 1.0 / shoe.getCards().size() * probability + probabilities.get(22));
             } else {
                 for (int i = dealerCard + 2; i <= 16 && i <= dealerCard + 11; i++) {
                     Shoe s = new Shoe(shoe);
                     if (i != dealerCard + 11) {
                         s.remove(i - dealerCard);
 
-                        dealerProbabilities(s, i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability, probabilities, false);
+                        dealerProbabilities(s, i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability, probabilities, false);
                     } else {
                         s.remove(1);
-                        dealerProbabilities(s, i, shoe.count(1)*1.0 / shoe.getCards().size() * probability, probabilities, true);
+                        dealerProbabilities(s, i, shoe.count(1) * 1.0 / shoe.getCards().size() * probability, probabilities, true);
                     }
                 }
                 for (int i = 17; i <= 21; i++) {
                     if (i != dealerCard + 11) {
-                        probabilities.put(i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability + probabilities.get(i));
+                        probabilities.put(i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability + probabilities.get(i));
                     } else {
-                        probabilities.put(i, shoe.count(1)*1.0 / shoe.getCards().size() * probability + probabilities.get(i));
+                        probabilities.put(i, shoe.count(1) * 1.0 / shoe.getCards().size() * probability + probabilities.get(i));
                     }
                 }
             }
@@ -65,18 +73,72 @@ public class Engine {
             for (int i = dealerCard + 1; i <= 16; i++) {
                 Shoe s = new Shoe(shoe);
                 s.remove(i - dealerCard);
-                dealerProbabilities(s, i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability, probabilities, true);
+                dealerProbabilities(s, i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability, probabilities, true);
             }
             for (int i = 17; i <= 21; i++) {
-                probabilities.put(i, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability + probabilities.get(i));
+                probabilities.put(i, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability + probabilities.get(i));
             }
             for (int i = 22; i <= dealerCard + 10; i++) {
                 Shoe s = new Shoe(shoe);
                 s.remove(i - dealerCard);
-                dealerProbabilities(s, i-10, shoe.count(i - dealerCard)*1.0 / shoe.getCards().size() * probability, probabilities, false);
+                dealerProbabilities(s, i - 10, shoe.count(i - dealerCard) * 1.0 / shoe.getCards().size() * probability, probabilities, false);
             }
         }
 
         return probabilities;
     }
+
+    public static double playerStandEV(HashMap<Integer, Double> probabilities, int playerSum) {
+        double ev = 0;
+
+        for (int i = 17; i <= 21; i++) {
+            if (playerSum > i) {
+                ev += probabilities.get(i);
+            } else if (playerSum < i) {
+                ev -= probabilities.get(i);
+            }
+        }
+        ev += probabilities.get(22);
+
+        return ev;
+    }
+
+    public static double playerDoubleEV(Shoe shoe, HashMap<Integer, Double> probabilities, int playerSum, boolean softHand) {
+        double ev = 0;
+
+        for (int i = playerSum + 1; i <= playerSum + 10; i++) {
+            if (i - playerSum != 1 || i >= 11) {
+                if (i >= 22 && !softHand) {
+                    ev -= shoe.count(i - playerSum) * 2.0 / shoe.getCards().size();
+                } else if (i >= 22) {
+                    ev += shoe.count(i - playerSum) * 2.0 / shoe.getCards().size() * playerStandEV(probabilities, i - 10);
+                } else {
+                    ev += shoe.count(i - playerSum) * 2.0 / shoe.getCards().size() * playerStandEV(probabilities, i);
+                }
+            } else {
+                ev += shoe.count(1) * 2.0 / shoe.getCards().size() * playerStandEV(probabilities, i + 10);
+            }
+        }
+
+        return ev;
+    }
+
+    public static double playerHitEV(Shoe shoe, HashMap<Integer, Double> probabilities, int playerSum, boolean softHand) {
+        double ev = 0;
+        //Hit ev = Hit ev oder Stand ev (je nach dem welches größer ist) mit jeder Karte
+        return ev;
+    }
+
+    public static double playerSplitEV(Shoe shoe, HashMap<Integer, Double> probabilities, int playerSum, boolean softHand) {
+        if(softHand) { return playerDoubleEV(shoe, probabilities, 11, true); }
+        return 2.0 * playerHitEV(shoe, probabilities, playerSum/2, false);
+    }
+
+    public static double insuranceEV(Shoe shoe) {
+        double ev = shoe.count(10) * 1.0 / shoe.getCards().size() - (shoe.getCards().size() - shoe.count(10)) * 1.0 / shoe.getCards().size();
+        if(ev > 0) { return ev; };
+        return 0;
+    }
+
+
 }
--- src/Strategy_Engine/Strategy_Engine.java
+++ src/Strategy_Engine/Strategy_Engine.java
@@ -1,17 +1,29 @@
 package Strategy_Engine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Strategy_Engine {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Observer o = new Observer(3);
 
-        System.out.println("-----------------------");
+        List<String> moves = new ArrayList<>();
+        moves.add("hit");
+        moves.add("stand");
+        moves.add("double");
+        moves.add("split");
+
+        //System.out.println("-----------------------");
 
         Observer o2 = new Observer(1);
 
         int dealerCard = 9;
 
+        o2.removeCard(dealerCard);
 
-        Engine.bestMove(o2.getShoe(), dealerCard, 5, false);
+        Engine.bestMove(o2.getShoe(), dealerCard, 15, false, moves);
     }
 
     public String bestMove(){
